0b539d6863e5b02a28c960fd138f5be1d50d46df
Felix_Weng <32306924+luckins@users.noreply.github.com>
2021-01-07 12:19:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #=================================================
 # Modify default IP
 # sed -i 's/15744/32448/g'
-sed -i 's/192.168.1.1/192.168.5.200/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.5.200/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
 sed -i 's/OpenWrt/R619ac/g' package/base-files/files/bin/config_generate
@@ -19,7 +19,7 @@ sed -i 's/OpenWrt/R619ac/g' package/base-files/files/bin/config_generate
 
 
 # 取消bootstrap为默认主题
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+# sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 # 删除原主题包
 rm -rf package/lean/luci-theme-argon
